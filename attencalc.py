85aeac236c573ffdb023eec5a4fa58a576d49506
charcmani <shubhanshu.085@gmail.com>
2017-07-02 10:15:16 +0530

    Py2 script for attendance calculator

--- attencalc.py
+++ attencalc.py
@@ -3,10 +3,11 @@ class CalcAtten(object):
 	"""docstring for CalcAtten"""
 	def __init__(self, p,t,pw):
 		super(CalcAtten, self).__init__()
-		self.present = int(p)
-		self.total = int(t)
-		self.per_week_class = int(pw)
-		self.cur_atten = (present/tot)*100
+		self.present = float(p)
+		self.total = float(t)
+		self.per_week_class = float(pw)
+		self.cur_atten = (self.present/self.total)*100
 	def req_lec(self):
 		res = math.ceil((self.cur_atten*(self.total+self.per_week_class))/100)
-		return res
+		res = res - self.present
+		return int(res)
